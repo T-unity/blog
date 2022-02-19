4eb5434bfd6fb5b61cabfa8f54a5679830e95909
T-unity <takadanaritoshi@gmail.com>
2022-02-19 16:40:36 +0900

    WIP

--- sample.php
+++ sample.php
@@ -3,14 +3,17 @@
 function echo_variable($var) {
   // echo $var;
   return $var;
-  // return 'called function';
 }
-
-echo_variable('test');
+// function echo_variable() {
+  // return 'called function';
+  // echo 'called function';
+// }
+// echo_variable('test');
 
 function this_is_callback( $callback_func ) {
   echo $callback_func('This is callback function');
-  // echo $callback_func();
+  // $callback_func('This is callback function');
+  // $callback_func();
 }
-
 this_is_callback('echo_variable');
+// this_is_callback('echo_variable');
