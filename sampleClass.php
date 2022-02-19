666ab46bfd09cf9e9278f6f39df0524332f9af97
T-unity <takadanaritoshi@gmail.com>
2022-02-19 19:43:58 +0900

    class

--- sampleClass.php
+++ sampleClass.php
@@ -1,9 +1,23 @@
 <?php
 class Employee {
+  public $name;
+  // public $state = '労働中';
+  private $state = '労働中';
+  public function getState() {
+    return $this->state;
+  }
+  public function setState($state) {
+    $this->state = $state;
+  }
   public function work($name) {
     echo `${name}はEmployeeクラスのworkメソッドとして働いています` . PHP_EOL;
+    // echo `${name}はEmployeeクラスのworkメソッドとして${state}` . PHP_EOL;
   }
 }
 
 $taro = new Employee;
-$taro->work('たろう');
+$taro->name = '田中';
+// echo $taro->state;
+// $taro->work($taro->name);
+$taro->setState('働きたくない');
+echo $taro->name , 'さんは' , $taro->getState() , PHP_EOL;
