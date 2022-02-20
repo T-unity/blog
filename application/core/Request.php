c391e8f3aabdbebb39dbcdac357344f2b75894ca
T-unity <takadanaritoshi@gmail.com>
2022-02-20 19:31:36 +0900

    request

---
+++ application/core/Request.php
@@ -0,0 +1,29 @@
+<?php
+
+class Request
+{
+  public function isPost()
+  {
+    //
+  }
+  public function getGet()
+  {
+    //
+  }
+  public function getPost()
+  {
+    //
+  }
+  public function getHost()
+  {
+    //
+  }
+  public function isSsl()
+  {
+    //
+  }
+  public function getRequestUri()
+  {
+    //
+  }
+}
