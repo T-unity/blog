b89b4c915818734ec822a4cd9cca439dba3f281f
T-unity <takadanaritoshi@gmail.com>
2022-03-03 23:13:47 +0900

    グローバル変数確認

--- php/src/index.php
+++ php/src/index.php
@@ -1,3 +1,18 @@
-<h1>Hello world</h1>
 <?php
-  echo 'Hello world';
+  echo '<h1>' . 'Hello world' . '</h1>';
+
+  // echo '<pre>';
+  // var_dump($GLOBALS);
+  // echo '</pre>';
+
+  // echo '<pre>';
+  // var_dump($_SERVER);
+  // echo '</pre>';
+
+  // echo '<pre>';
+  // var_dump($_REQUEST);
+  // echo '</pre>';
+
+  // echo '<pre>';
+  // var_dump($_COOKIE);
+  // echo '</pre>';
