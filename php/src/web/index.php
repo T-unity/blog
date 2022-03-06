0ea85e3ab32fff5dc0e6591f624daa2c60e94005
T-unity <takadanaritoshi@gmail.com>
2022-03-06 18:05:59 +0900

    インスタンスが生成できる事を確認

--- php/src/web/index.php
+++ php/src/web/index.php
@@ -1,18 +1,9 @@
 <?php
-  echo '<h1>' . 'Hello world' . '</h1>';
+  echo '<h1>' . 'Document root' . '</h1>';
 
-  // echo '<pre>';
-  // var_dump($GLOBALS);
-  // echo '</pre>';
+  require( '../core/Request.php' );
 
-  // echo '<pre>';
+  $req = new Request();
+
   // var_dump($_SERVER);
-  // echo '</pre>';
-
-  // echo '<pre>';
-  // var_dump($_REQUEST);
-  // echo '</pre>';
-
-  // echo '<pre>';
-  // var_dump($_COOKIE);
-  // echo '</pre>';
+  var_dump($req);
