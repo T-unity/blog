dda28f0409b465cefbb6056f57aaa66f49599922
T-unity <takadanaritoshi@gmail.com>
2022-03-06 18:33:15 +0900

    POSTメソッドから値を取得するメソッドを作成

--- php/src/core/Request.php
+++ php/src/core/Request.php
@@ -18,13 +18,39 @@ class Request
   }
 
   /**
-   * URLパラメータから渡される連想配列
+   * HTTP GET メソッドから渡される連想配列
    * https://www.php.net/manual/ja/reserved.variables.get.php
    */
   public function getGet( $name, $default = null )
   {
     if ( isset( $_GET[$name] ) ) {
       return $_GET[$name];
+    }
+
+    return $default;
+  }
+
+  /**
+   * HTTP POST メソッドから渡される連想配列
+   * https://www.php.net/manual/ja/reserved.variables.post.php
+   */
+  public function getPost( $name, $default = null )
+  {
+    if ( isset( $_POST[$name] ) ) {
+      return $_POST[$name];
+    }
+
+    return $default;
+  }
+
+    /**
+   * HTTP POST メソッドから渡される連想配列
+   * https://www.php.net/manual/ja/reserved.variables.post.php
+   */
+  public function getHost( $name, $default = null )
+  {
+    if ( isset( $_POST[$name] ) ) {
+      return $_POST[$name];
     }
 
     return $default;
--- php/src/web/index.php
+++ php/src/web/index.php
@@ -5,6 +5,7 @@
 
   $req = new Request();
 
-  var_dump($_GET);
+  // var_dump($_GET);
+  var_dump($_POST);
   // var_dump($_SERVER);
   // var_dump($req->getGet('get'));
